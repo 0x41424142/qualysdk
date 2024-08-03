69022216b82dd886efbb19bd6f330c394ce248e4
0x41424142 <89279733+0x41424142@users.noreply.github.com>
2024-08-03 13:49:14 +0000

    :art: Format Python code with psf/black

--- qualyspy/vmdr/reports.py
+++ qualyspy/vmdr/reports.py
@@ -13,14 +13,19 @@ from ..base import xml_parser
 from .data_classes.lists import BaseList
 from ..exceptions.Exceptions import *
 
-def manage_reports(auth: BasicAuth, action: Literal["list", "launch", "cancel", "fetch", "delete"], **kwargs) -> Response:
+
+def manage_reports(
+    auth: BasicAuth,
+    action: Literal["list", "launch", "cancel", "fetch", "delete"],
+    **kwargs,
+) -> Response:
     """
     Backend function to manage reports in Qualys VMDR.
 
     Parameters:
         ```auth```: ```Required[BasicAuth]``` - The BasicAuth object.
         ```action```: ```Literal["list", "launch", "cancel", "fetch", "delete"]``` - The action to take.
-    
+
     Returns:
         ```Response``` - The response from the API.
 
@@ -36,10 +41,10 @@ def manage_reports(auth: BasicAuth, action: Literal["list", "launch", "cancel", 
     """
 
     # Set specific kwargs
-    kwargs['action'] = action
-    kwargs['echo_request'] = False
+    kwargs["action"] = action
+    kwargs["echo_request"] = False
 
-    headers = {'X-Requested-With': 'qualyspy SDK'}
+    headers = {"X-Requested-With": "qualyspy SDK"}
 
     match action:
         case "list":
@@ -52,7 +57,6 @@ def manage_reports(auth: BasicAuth, action: Literal["list", "launch", "cancel", 
             )
         case _:
             raise NotImplementedError(f"Action {action} is not implemented yet.")
-        
 
 
 def get_report_list(auth: BasicAuth, **kwargs) -> BaseList[VMDRReport]:
@@ -71,14 +75,14 @@ def get_report_list(auth: BasicAuth, **kwargs) -> BaseList[VMDRReport]:
         ```client_name```: ```Optional[str]``` - Filter output to reports for a specific client name. ONLY VALID FOR CONSULTANT SUBSCRIPTIONS!
 
     Returns:
-        ```BaseList[VMDRReport]``` - A list of VMDRReport objects.  
+        ```BaseList[VMDRReport]``` - A list of VMDRReport objects.
     """
 
     response = manage_reports(auth, action="list", **kwargs)
 
     data = xml_parser(response.text)
 
-    reports = data['REPORT_LIST_OUTPUT']['RESPONSE']['REPORT_LIST']['REPORT']
+    reports = data["REPORT_LIST_OUTPUT"]["RESPONSE"]["REPORT_LIST"]["REPORT"]
 
     bl = BaseList()
 

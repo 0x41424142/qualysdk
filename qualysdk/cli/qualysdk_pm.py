4b5452c654f925e0d9d24f72c1aab8515841e267
Jake Lindsay <jake@jakelindsay.uk>
2024-11-16 14:24:14 -0500

    Added get_job_runs to CLI tool

--- qualysdk/cli/qualysdk_pm.py
+++ qualysdk/cli/qualysdk_pm.py
@@ -24,6 +24,8 @@ def cli_fn(auth: TokenAuth, args: Namespace, endpoint: str) -> None:
             result = list_jobs(auth, **kwargs)
         case "get_job_results":
             result = get_job_results(auth, args.job_id, **kwargs)
+        case "get_job_runs":
+            result = get_job_runs(auth, args.job_id, **kwargs)
         case _:
             raise ValueError(f"Invalid endpoint: {endpoint}.")
 
@@ -100,6 +102,31 @@ def main():
         metavar=("key", "value"),
     )
 
+    job_runs_parser = subparsers.add_parser(
+        "get_job_runs", help="Get runs for a PM job."
+    )
+    job_runs_parser.add_argument(
+        "-o",
+        "--output",
+        help="Output xlsx file to write results to",
+        type=str,
+        default="pm_job_{JOB_ID}_runs.xlsx",
+    )
+    job_runs_parser.add_argument(
+        "-j",
+        "--job-id",
+        help="Specify the job ID to get runs for",
+        type=str,
+        required=True,
+    )
+    job_runs_parser.add_argument(
+        "--kwarg",
+        help="Specify a keyword argument to pass to the action. Can be used multiple times",
+        action="append",
+        nargs=2,
+        metavar=("key", "value"),
+    )
+
     args = parser.parse_args()
 
     # create TokenAuth object
@@ -111,6 +138,9 @@ def main():
         case "get_job_results":
             args.output = args.output.replace("{JOB_ID}", args.job_id)
             cli_fn(auth=auth, args=args, endpoint="get_job_results")
+        case "get_job_runs":
+            args.output = args.output.replace("{JOB_ID}", args.job_id)
+            cli_fn(auth=auth, args=args, endpoint="get_job_runs")
         case _:
             parser.print_help()
             exit(1)

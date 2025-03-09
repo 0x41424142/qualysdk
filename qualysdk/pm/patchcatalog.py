16445894aac98b9db2e8012f5ade9a149523b09a
0x41424142 <89279733+0x41424142@users.noreply.github.com>
2025-03-09 20:59:38 +0000

    :art: Format Python code with psf/black

--- qualysdk/pm/patchcatalog.py
+++ qualysdk/pm/patchcatalog.py
@@ -22,7 +22,8 @@ def get_patch_catalog(
     patchId: Union[int, str],
     platform: Literal["windows", "linux"] = "windows",
     **kwargs,
-) -> BaseList[CatalogPatch]: ...
+) -> BaseList[CatalogPatch]:
+    ...
 
 
 @overload
@@ -31,7 +32,8 @@ def get_patch_catalog(
     patchId: Union[BaseList[str, int], list[str, int]],
     platform: Literal["windows", "linux"] = "windows",
     **kwargs,
-) -> BaseList[CatalogPatch]: ...
+) -> BaseList[CatalogPatch]:
+    ...
 
 
 def get_patch_catalog(
@@ -250,7 +252,8 @@ def validate_threads_and_patches(patchId, threads):
 @overload
 def get_packages_in_linux_patch(
     auth: TokenAuth, patchId: str, threads: int = 5, **kwargs
-) -> BaseList[PackageDetail]: ...
+) -> BaseList[PackageDetail]:
+    ...
 
 
 @overload
@@ -259,7 +262,8 @@ def get_packages_in_linux_patch(
     patchId: Union[BaseList[str], list[str]],
     threads: int = 5,
     **kwargs,
-) -> BaseList[PackageDetail]: ...
+) -> BaseList[PackageDetail]:
+    ...
 
 
 def get_packages_in_linux_patch(
@@ -323,7 +327,8 @@ def get_products_in_windows_patch(
     auth: TokenAuth,
     patchId: str,
     threads: int = 5,
-) -> BaseList[AssociatedProduct]: ...
+) -> BaseList[AssociatedProduct]:
+    ...
 
 
 @overload
@@ -331,7 +336,8 @@ def get_products_in_windows_patch(
     auth: TokenAuth,
     patchId: Union[BaseList[str], list[str]],
     threads: int = 5,
-) -> BaseList[AssociatedProduct]: ...
+) -> BaseList[AssociatedProduct]:
+    ...
 
 
 def get_products_in_windows_patch(
@@ -393,7 +399,8 @@ def count_product_vulns(
         List[Literal["Critical", "Important", "Moderate", "Low", "None"]],
     ] = None,
     tagUUIDs: str = None,
-) -> BaseList[ProductVulnCount]: ...
+) -> BaseList[ProductVulnCount]:
+    ...
 
 
 @overload
@@ -404,7 +411,8 @@ def count_product_vulns(
         List[Literal["Critical", "Important", "Moderate", "Low", "None"]],
     ] = None,
     tagUUIDs: List[str] = None,
-) -> BaseList[ProductVulnCount]: ...
+) -> BaseList[ProductVulnCount]:
+    ...
 
 
 def count_product_vulns(
